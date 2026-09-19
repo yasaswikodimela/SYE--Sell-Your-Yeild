e8b808869dc8fdbf7d6f3a3684d224f89f2050cb
yasaswikodimela <yasaswikodimela5@gmail.com>
2026-09-19 05:53:53 +0530

    Fix Android emulator backend connection

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,9 +1,16 @@
 import 'dart:convert';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:http/http.dart' as http;
 
 class ApiService {
   // FastAPI backend
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static String get baseUrl {
+    if (!kIsWeb && Platform.isAndroid) {
+      return 'http://10.0.2.2:8000';
+    }
+    return 'http://127.0.0.1:8000';
+  }
 
   static Future<String> testConnection() async {
     final response = await http.get(Uri.parse('$baseUrl/'));
